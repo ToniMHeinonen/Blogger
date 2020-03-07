4879c0ee0b5a180735ae8cfaa2ee4dd1ec547716
Toni <toni.heinonen@hotmail.fi>
2020-03-07 10:48:22 +0200

    Backend: Add necessary information to BlogPost Entity.

--- backend/src/main/java/io/github/tonimheinonen/blogger/BlogPost.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/BlogPost.java
@@ -1,8 +1,57 @@
 package io.github.tonimheinonen.blogger;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class BlogPost {
+    @Id
+    @GeneratedValue
+    private long id;
 
+    private String topic;
+    private String text;
+    private Date creationDate;
+
+    public BlogPost(String topic, String text, Date creationDate) {
+        this.topic = topic;
+        this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPost [creationDate=" + creationDate + ", id=" + id + ", text=" + text + ", topic=" + topic + "]";
+    }
 }
