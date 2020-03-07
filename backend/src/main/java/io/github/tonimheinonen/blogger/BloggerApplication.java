961312f054b77e75233e579c664692cc7069386e
Toni <toni.heinonen@hotmail.fi>
2020-03-07 10:26:40 +0200

    Print creators and ascii art when starting backend.

--- backend/src/main/java/io/github/tonimheinonen/blogger/BloggerApplication.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/BloggerApplication.java
@@ -1,13 +1,18 @@
 package io.github.tonimheinonen.blogger;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BloggerApplication {
 
+	private static Log logger = LogFactory.getLog(BloggerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BloggerApplication.class, args);
+		logger.info("Creators: Olli Ahonen & Toni Heinonen");
 	}
 
 }
