462f234612cd2362b954f22244f5692d24a70153
Toni <toni.heinonen@hotmail.fi>
2020-03-07 17:53:09 +0200

    Backend: Add possibility to get certain blog post.

--- backend/src/main/java/io/github/tonimheinonen/blogger/MyRestController.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/MyRestController.java
@@ -1,6 +1,7 @@
 package io.github.tonimheinonen.blogger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,10 @@ public class MyRestController {
     @RequestMapping(value = "/blogposts", method= RequestMethod.GET)
     public Iterable<BlogPost> fetchBlogposts() {
         return database.findAll();
+    }
+
+    @RequestMapping(value = "/blogposts/{blogId}", method= RequestMethod.GET)
+    public BlogPost fetchCustomer(@PathVariable long blogId) {
+        return database.findById(blogId).get();
     }
 }
