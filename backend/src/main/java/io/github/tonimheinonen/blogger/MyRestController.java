fdd93ad0e2a946c4d0674393c30ccb59642b3469
Toni <toni.heinonen@hotmail.fi>
2020-03-07 11:21:11 +0200

    Backend: Add Get for getting blog posts.

--- backend/src/main/java/io/github/tonimheinonen/blogger/MyRestController.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/MyRestController.java
@@ -1,8 +1,18 @@
 package io.github.tonimheinonen.blogger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
+    @Autowired
+    BlogPostRepository database;
+
+    @RequestMapping(value = "/blogposts", method= RequestMethod.GET)
+    public Iterable<BlogPost> fetchBlogposts() {
+        return database.findAll();
+    }
 }
