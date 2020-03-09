53d393ab0f139f3ae7083d6c04177352cec6be16
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-09 16:58:19 +0200

    Frontend: Add id's to all blog posts.

--- frontend/src/AllPosts.js
+++ frontend/src/AllPosts.js
@@ -5,7 +5,8 @@ function AllPosts(props) {
     let data = []
     for (let i = 0; i < props.amount; i++) {
         data.push(<Post 
-            key={props.allBlogPosts[i].id} 
+            key={props.allBlogPosts[i].id}
+            id={props.allBlogPosts[i].id}
             topic={props.allBlogPosts[i].topic} 
             creationDate={props.allBlogPosts[i].creationDate} 
             text={props.allBlogPosts[i].text}>
