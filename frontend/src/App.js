91b69c04f537e79d5ae2cbf3d4baf6e24c04ee86
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-09 09:48:59 +0200

    Frontend: Add proxy to package.json
    Testing if the fetching works. Not sure if the proxy-thing is the correct way to do this. It gives cors-error, if proxy isn't added to package.json.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [blogPost, setBlogPost] = React.useState([])
+
+  async function fetchBlogPost() {
+    const hr = await fetch('/blogposts/1/')
+    const json = await hr.json()
+    const result = await json.text
+    setBlogPost(result)
+  }
+
+  React.useEffect(() => {
+    fetchBlogPost()
+  })
+
   return (
-    <div className="App">
+    <div>
+      {blogPost}
     </div>
   );
 }
