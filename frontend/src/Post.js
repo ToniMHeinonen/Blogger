0c2957a6dead559ef2abedc4a5f27d0432213a34
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-09 16:58:36 +0200

    Frontend: Add ability to delete posts.

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 function Post(props) {
+  const deleted = (event) => {
+    fetch('/blogposts/' + props.id, {
+      method: 'DELETE',
+    })
+    .then(window.location.reload())
+  }
+
   return (
     <div>
     <h2>{props.topic}</h2>
     <h3>{props.creationDate}</h3>
     <p>{props.text}</p>
     <button>Edit</button>
-    <button>Delete</button>
+    <button onClick={deleted}>Delete</button>
     </div>
     )
 }
