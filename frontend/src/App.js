781bf0fb1dc488def68539c322d437f756bd3ac0
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-10 16:28:22 +0200

    Frontend: Setup router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 import Post from './Post'
 import AllPosts from './AllPosts'
+import AddPost from './AddPost'
+import Home from './Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
-  const [blogPosts, setBlogPosts] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(false)
-
-  async function fetchBlogPosts() {
-    setIsLoading(true)
-    const hr = await fetch('/blogposts/')
-    const json = await hr.json()
-    setBlogPosts(json)
-    setIsLoading(false)
-  }
-
-  React.useEffect(() => {
-    fetchBlogPosts()
-  }, [])
-
-  const addPost = async () => {
-    const newPost = { topic: 'hello', text: 'uutta tekstiä' }
-    const conf = {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(newPost)
-    }
-    await fetch('/blogposts/', conf)
-    window.location.reload()
-  }
 
   return (
-    <div>
-      <button onClick={addPost}>Add</button>
-      {isLoading ? 'Loading...' : <AllPosts allBlogPosts={blogPosts} amount={blogPosts.length}/>}
-    </div>
+    <Router>
+      <div>
+        <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/addPost">Add new post</Link>
+              </li>
+            </ul>
+          </nav>
+        <Switch>
+          <Route path="/addPost">
+            <AddPost />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
