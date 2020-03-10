e2e0ca8627cde6eec811d7f9acb04e1036eb38f4
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-10 16:28:40 +0200

    Frontend: Add new Home-function.

---
+++ frontend/src/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import Post from './Post';
+import AllPosts from './AllPosts';
+import AddPost from './AddPost';
+
+function Home() {
+  const [blogPosts, setBlogPosts] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  async function fetchBlogPosts() {
+    setIsLoading(true)
+    const hr = await fetch('/blogposts/')
+    const json = await hr.json()
+    setBlogPosts(json)
+    setIsLoading(false)
+  }
+
+  React.useEffect(() => {
+    fetchBlogPosts()
+  }, [])
+
+  const addPost = async () => {
+    const newPost = { topic: 'hello', text: 'uutta tekstiä' }
+    const conf = {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(newPost)
+    }
+    await fetch('/blogposts/', conf)
+    window.location.reload()
+  }
+
+  return (
+    <div>
+    <button onClick={addPost}>Add</button>
+    {isLoading ? 'Loading...' : <AllPosts allBlogPosts={blogPosts} amount={blogPosts.length}/>}
+  </div>
+  )
+}
+
+export default Home
