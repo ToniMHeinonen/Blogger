f0cb2dc0c6bde1564fb7b25c721c8826c57d72ef
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-20 18:33:58 +0200

    Add ability to modify blogposts.

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 function Post(props) {
+  const [redirect, setRedirect] = React.useState(false)
+
   const deleted = (event) => {
     fetch('/blogposts/' + props.id, {
       method: 'DELETE',
@@ -8,12 +11,25 @@ function Post(props) {
     .then(window.location.reload())
   }
 
+  const edited = (event) => {
+    event.preventDefault()
+    setRedirect(true)
+  }
+
+  if (redirect) {
+    return <Redirect to={{
+      pathname: '/modifyPost',
+      state: { id: props.id, topic: props.topic, text: props.text }
+  }}
+/>
+  }
+
   return (
     <div>
     <h2>{props.topic}</h2>
     <h3>{props.creationDate}</h3>
     <p>{props.text}</p>
-    <button>Edit</button>
+    <button onClick={edited}>Edit</button>
     <button onClick={deleted}>Delete</button>
     </div>
     )
