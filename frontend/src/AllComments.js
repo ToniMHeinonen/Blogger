7e40271cd6b2e9c1ccd441766675827a6e716b70
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-31 16:45:00 +0300

    Create AllComments.js.

---
+++ frontend/src/AllComments.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Comment from './Comment';
+
+function AllComments(props) {
+    let data = []
+    for (let i = 0; i < props.amount; i++) {
+        data.push(<Comment 
+            key={props.allComments[i].id}
+            id={props.allComments[i].id}
+            author={props.allComments[i].author}
+            creationDate={props.allComments[i].creationDate} 
+            text={props.allComments[i].text}
+            likes={props.allComments[i].likes}>
+            </Comment>)
+    }
+
+    return <div>{data}</div>
+}
+
+export default AllComments
