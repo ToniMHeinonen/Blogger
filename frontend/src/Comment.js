f361e599b3d19f0fcd447320446b7db01244a6f5
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-01 09:16:31 +0300

    Frontend: Add ability to delete comments.

--- frontend/src/Comment.js
+++ frontend/src/Comment.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 function Comment(props) {
+  const [sending, isSending] = React.useState(false)
+
+  // Delete clicked comment.
+  const deleted = async (event) => {
+    isSending(true)
+    await fetch('/comments/' + props.id, {
+      method: 'DELETE',
+    })
+    window.location.reload()
+  }
+
   return (
     <div>
     <h5>Author: {props.author} <br/>Created: {props.creationDate}
     <br/>Likes: {props.likes}</h5>
     <p>{props.text}</p>
+    <button disabled={sending} onClick={deleted}>Delete comment</button>
     <br/>
     </div>
   )
