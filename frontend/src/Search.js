9ac7dca54902d52f29ebe38b5fd15f67637dfd90
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-03 14:50:08 +0300

    Frontend: Add ability to search comments.

--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -1,42 +1,84 @@
 import React from 'react'
-import Post from './Post'
 import AllPosts from './AllPosts'
+import SearchResult from './SearchResult'
+import AllComments from './AllComments'
 
 function Search() {
   const [sending, isSending] = React.useState(false)
-  const [result, setResult] = React.useState([])
-  const [showNothing, setShowNothing] = React.useState(true)
+  const [blogResult, setBlogResult] = React.useState([])
+  const [hideBlogResult, setHideBlogResult] = React.useState(true)
+  const [showBlogChoice, setShowBlogChoice] = React.useState(true)
+  const [blogPosts, setBlogPosts] = React.useState([])
+  const [commentResult, setCommentResult] = React.useState([])
+  const [hideCommentResult, setHideCommentResult] = React.useState(true)
 
+  // Fetch blogposts.
+  async function fetchBlogPosts() {
+    const hr = await fetch('/blogposts/')
+    const json = await hr.json()
+    setBlogPosts(json)
+  }
+
+  // Get data from the form and fetch blogposts/comments.
   const search = async (event) => {
     isSending(true)
     event.preventDefault()
     const data = new FormData(event.target)
-    const newPost = { searchFrom: data.get('items'), searchText: data.get('searchText') }
+    const newPost = { searchFrom: data.get('items'), searchText: data.get('searchText'), blogID: data.get('blogID') }
     if (newPost.searchText !== '' && newPost.searchText.length < 30) {
-      const hr = await fetch(`/${newPost.searchFrom}/search/${newPost.searchText}`)
-      const json = await hr.json()
-      setResult(json)
+      if (newPost.searchFrom === 'blogposts') {
+        const hr = await fetch(`/${newPost.searchFrom}/search/${newPost.searchText}`)
+        const json = await hr.json()
+        setBlogResult(json)
+        setHideBlogResult(false)
+        setHideCommentResult(true)
+      } else if (newPost.searchFrom === 'comments') {
+        const hr = await fetch(`/comments/${newPost.blogID}/search/${newPost.searchText}`)
+        const json = await hr.json()
+        setCommentResult(json)
+        setHideCommentResult(false)
+        setHideBlogResult(true)
+      }
     }
     isSending(false)
-    setShowNothing(false)
   }
+
+  // Chech if comments are selected from dropdown-list and show/hide blog-choice.
+  const check = (event) => {
+    if (event.target.value === 'comments') {
+      setShowBlogChoice(false)
+    } else {
+      setShowBlogChoice(true)
+    }
+  }
+
+  // Fetch blogposts, when mounted.
+  React.useEffect(() => {
+    fetchBlogPosts()
+  }, [])
 
   return (
     <div>
     <form onSubmit={search}>
       <label>Search from: </label>
-      <select id="items" name="items">
+      <select id="items" name="items" onChange={check}>
         <option value="blogposts">Blogposts</option>
         <option value="comments">Comments</option>
       </select>
       <br/>
+      {showBlogChoice ? null : [
+      <label key="search">Search comments from blog:</label>,
+      <SearchResult key="posts" blogPosts={blogPosts} amount={blogPosts.length}/>,
+      <br key="space"/>
+      ]}
       <label>
         Text (1-29 characters):
         <input id="searchText" name="searchText" type="text"/>
       </label>
       <button disabled={sending}>Send</button>
     </form>
-    {showNothing ? '' : <AllPosts allBlogPosts={result} amount={result.length}/>}
+    {hideBlogResult ? null : <AllPosts allBlogPosts={blogResult} amount={blogResult.length}/>}
+    {hideCommentResult ? null : <AllComments allComments={commentResult} amount={commentResult.length}/>}
     </div>
   )
 }
