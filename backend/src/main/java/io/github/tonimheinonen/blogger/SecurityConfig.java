14089cab9f686ae7977e23e91495a1dbf4d9004e
Toni <toni.heinonen@hotmail.fi>
2020-04-04 14:52:19 +0300

    Backend: Add basic authentication to some urls.

---
+++ backend/src/main/java/io/github/tonimheinonen/blogger/SecurityConfig.java
@@ -0,0 +1,46 @@
+package io.github.tonimheinonen.blogger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ 
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                //.antMatchers(HttpMethod.GET, "/blogposts").hasRole("USER")
+                //.antMatchers(HttpMethod.GET, "/blogposts/**").hasRole("USER")
+                //.antMatchers(HttpMethod.GET, "/blogposts/search/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/blogposts").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/blogposts/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/blogposts/**").hasRole("ADMIN")
+                //.antMatchers(HttpMethod.GET, "/comments/**").hasRole("USER")
+                //.antMatchers(HttpMethod.GET, "/comments/**/search/**").hasRole("USER")
+                //.antMatchers(HttpMethod.POST, "/comments/**").hasRole("USER")
+                //.antMatchers(HttpMethod.POST, "/comments/like/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/comments/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+  
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication()
+                //.withUser("user").password("{noop}password").roles("USER")
+                //.and()
+                .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
+    }
+}
