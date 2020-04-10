42d21589d2db1fc12e07087a59a6f6607982854b
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-10 09:26:18 +0300

    Frontend: Give error-message, if user is trying to access modifyPost without logged in..

--- frontend/src/ModifyPost.js
+++ frontend/src/ModifyPost.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+import Cookies from 'universal-cookie'
 
 function ModifyPost(props) {
   const [redirect, setRedirect] = React.useState(false)
   const [sending, isSending] = React.useState(false)
+  const cookies = new Cookies()
 
   // Post modified blogpost.
   const send = async (event) => {
@@ -25,26 +27,34 @@ function ModifyPost(props) {
     return <Redirect push to="/"/>
   }
 
+  if (cookies.get('authCookie') === 'true') {
+    return (
+      <form onSubmit={send}>
+        <label>
+          Topic:
+          <br/>
+          <input id="topic" name="topic" type="text" defaultValue={props.location.state.topic}/>
+        </label>
+        <br/><br/>
+        <label>
+          Author:
+          <br/>
+          <input id="author" name="author" type="text" defaultValue={props.location.state.author}/>
+        </label>
+        <br/><br/>
+        Your story:
+        <br/>
+        <textarea id="blogi" name="blogi" type="text" defaultValue={props.location.state.text}/>
+        <br/><br/>
+        <button disabled={sending}>Send</button>
+      </form>
+    )
+  }
+
   return (
-    <form onSubmit={send}>
-      <label>
-        Topic:
-        <br/>
-        <input id="topic" name="topic" type="text" defaultValue={props.location.state.topic}/>
-      </label>
-      <br/><br/>
-      <label>
-        Author:
-        <br/>
-        <input id="author" name="author" type="text" defaultValue={props.location.state.author}/>
-      </label>
-      <br/><br/>
-      Your story:
-      <br/>
-      <textarea id="blogi" name="blogi" type="text" defaultValue={props.location.state.text}/>
-      <br/><br/>
-      <button disabled={sending}>Send</button>
-    </form>
+    <div>
+      You are not allowed to edit posts.
+    </div>
   )
 }
 
