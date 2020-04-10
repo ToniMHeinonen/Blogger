351eeb11e30b6f3155921f49b26fde35efdcb4b8
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-10 09:29:21 +0300

    Frontend: Give error-message also when adding new posts.

--- frontend/src/AddPost.js
+++ frontend/src/AddPost.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+import Cookies from 'universal-cookie'
 
 function AddPost() {
   const [redirect, setRedirect] = React.useState(false)
   const [sending, isSending] = React.useState(false)
+  const cookies = new Cookies()
 
   // Called, when user have pressed send-button. Get data from form and post to backend.
   const send = async (event) => {
@@ -25,26 +27,34 @@ function AddPost() {
     return <Redirect push to="/"/>
   }
 
+  if (cookies.get('authCookie') === 'true') {
+    return (
+      <form onSubmit={send}>
+        <label>
+          Topic:
+          <br/>
+          <input id="topic" name="topic" type="text"/>
+        </label>
+        <br/><br/>
+        <label>
+          Author:
+          <br/>
+          <input id="author" name="author" type="text"/>
+        </label>
+        <br/><br/>
+        Your story:
+        <br/>
+        <textarea id="blogi" name="blogi" type="text"/>
+        <br/><br/>
+        <button disabled={sending}>Send</button>
+      </form>
+    )
+  }
+
   return (
-    <form onSubmit={send}>
-      <label>
-        Topic:
-        <br/>
-        <input id="topic" name="topic" type="text"/>
-      </label>
-      <br/><br/>
-      <label>
-        Author:
-        <br/>
-        <input id="author" name="author" type="text"/>
-      </label>
-      <br/><br/>
-      Your story:
-      <br/>
-      <textarea id="blogi" name="blogi" type="text"/>
-      <br/><br/>
-      <button disabled={sending}>Send</button>
-    </form>
+    <div>
+      You are not allowed to add posts.
+    </div>
   )
 }
 
