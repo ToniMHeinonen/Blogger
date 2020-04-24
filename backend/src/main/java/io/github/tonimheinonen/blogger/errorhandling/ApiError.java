fd00cf661e28f06abcd922146d2840573addfffb
Toni <toni.heinonen@hotmail.fi>
2020-04-24 15:10:01 +0300

    Backend: Add javadoc to ApiError.

--- backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/ApiError.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/ApiError.java
@@ -7,6 +7,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Represents an error message.
+ * @author Toni Heinonen
+ * @author toni1.heinonen@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 class ApiError {
 
     private HttpStatus status;
@@ -16,15 +23,27 @@ class ApiError {
     private String debugMessage;
     private List<ApiSubError> subErrors;
  
+    /**
+     * Private constructor for setting timestamp to current time.
+     */
     private ApiError() {
         timestamp = LocalDateTime.now();
     }
  
+    /**
+     * Constructor for setting http status.
+     * @param status status to set
+     */
     ApiError(HttpStatus status) {
         this();
         this.status = status;
     }
  
+    /**
+     * Constructor for setting unexpected error message.
+     * @param status https status
+     * @param ex expection
+     */
     ApiError(HttpStatus status, Throwable ex) {
         this();
         this.status = status;
@@ -32,6 +51,12 @@ class ApiError {
         this.debugMessage = ex.getLocalizedMessage();
     }
  
+    /**
+     * Constructor for setting error with custom message.
+     * @param status http status
+     * @param message custom message
+     * @param ex exception
+     */
     ApiError(HttpStatus status, String message, Throwable ex) {
         this();
         this.status = status;
@@ -39,42 +64,82 @@ class ApiError {
         this.debugMessage = ex.getLocalizedMessage();
     }
 
+    /**
+     * Returns http status.
+     * @return http status
+     */
     public HttpStatus getStatus() {
         return status;
     }
 
+    /**
+     * Sets http status.
+     * @param status http status
+     */
     public void setStatus(HttpStatus status) {
         this.status = status;
     }
 
+    /**
+     * Returns error timestamp.
+     * @return error timestamp
+     */
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Sets error timestamp.
+     * @param timestamp error timestamp
+     */
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
     }
 
+    /**
+     * Returns error message.
+     * @return error message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets error message.
+     * @param message error message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Returns debug message.
+     * @return debug message
+     */
     public String getDebugMessage() {
         return debugMessage;
     }
 
+    /**
+     * Sets debug message.
+     * @param debugMessage debug message
+     */
     public void setDebugMessage(String debugMessage) {
         this.debugMessage = debugMessage;
     }
 
+    /**
+     * Returns sub errors.
+     * @return sur errors
+     */
     public List<ApiSubError> getSubErrors() {
         return subErrors;
     }
 
+    /**
+     * Sets sub errors.
+     * @param subErrors sub errors
+     */
     public void setSubErrors(List<ApiSubError> subErrors) {
         this.subErrors = subErrors;
     }
