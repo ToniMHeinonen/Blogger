ac126f635c8edc83d7055b1a61c0c63cb357ea3a
Toni <toni.heinonen@hotmail.fi>
2020-04-24 11:38:26 +0300

    Backend: Add javadoc to BlogPostRepository & CommentRepository.

--- backend/src/main/java/io/github/tonimheinonen/blogger/BlogPostRepository.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/BlogPostRepository.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Handles Blog Post repository calls.
+ * @author Toni Heinonen
+ * @author toni1.heinonen@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+    
+    /**
+     * Searches blog posts by provided topic and text.
+     * @param topic topic to search
+     * @param text text content to search
+     * @return fetched blog posts
+     */
     List<BlogPost> findByTopicContainsIgnoreCaseOrTextContainsIgnoreCase(String topic, String text);
 }
--- backend/src/main/java/io/github/tonimheinonen/blogger/CommentRepository.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/CommentRepository.java
@@ -4,8 +4,33 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Handles Comment repository calls.
+ * @author Toni Heinonen
+ * @author toni1.heinonen@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 interface CommentRepository extends CrudRepository<Comment, Long> {
+    
+    /**
+     * Searched comments by provided blog post id.
+     * @param id blog id the comment needs to be connected to
+     * @return
+     */
     List<Comment> findByBlogPostId(Long id);
+
+    /**
+     * Removes all comments connected to provided blog post id.
+     * @param id blog id the comment needs to be connected to
+     */
     void removeAllByBlogPostId(Long id);
+
+    /**
+     * Searches comments by provided blog post id and text.
+     * @param id blog id the comment needs to be connected to
+     * @param text text content to search
+     * @return fetched comments
+     */
     List<Comment> findByBlogPostIdAndTextContainsIgnoreCase(Long id, String text);
 }
