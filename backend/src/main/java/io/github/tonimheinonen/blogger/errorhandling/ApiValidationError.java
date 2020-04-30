79ea4cd874dfd9dc81b0a953846654afad33ff6b
Toni <toni.heinonen@hotmail.fi>
2020-04-30 12:48:22 +0300

    Backend: Add javadoc to ApiValidationError.

--- backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/ApiValidationError.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/ApiValidationError.java
@@ -4,6 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * Handles creating api validation errors.
+ * @author Toni Heinonen
+ * @author toni1.heinonen@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
@@ -13,6 +20,11 @@ class ApiValidationError {
    private Object rejectedValue;
    private String message;
 
+   /**
+    * Creates an ApiValidationError.
+    * @param object name of the object with failed validation
+    * @param message error message
+    */
    ApiValidationError(String object, String message) {
        this.object = object;
        this.message = message;
