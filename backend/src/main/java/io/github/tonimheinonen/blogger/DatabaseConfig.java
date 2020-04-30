96a169ca1c92aec2c3daec3714c6589880fdf5a2
Toni <toni.heinonen@hotmail.fi>
2020-04-30 13:27:17 +0300

    Backend: Remove fix for 'max_user_correction' error.
    Fix apparently did not work.

--- backend/src/main/java/io/github/tonimheinonen/blogger/DatabaseConfig.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/DatabaseConfig.java
@@ -29,11 +29,6 @@ public class DatabaseConfig {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
 
-        // These probably fix the user 'root' has exceeded the 'max_user_connections' resource spring boot,
-        // it's hard to test since the problem is hard to reproduce
-        config.setMaxLifetime(30000);
-        config.setMaximumPoolSize(10);
-
         return new HikariDataSource(config);
     }
 }
