a828ff1533f395653d0e40708a41c8faf140c08b
Toni <toni.heinonen@hotmail.fi>
2020-04-30 08:37:26 +0300

    Backend: Add javadoc to RestExceptionHandler.

--- backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/RestExceptionHandler.java
+++ backend/src/main/java/io/github/tonimheinonen/blogger/errorhandling/RestExceptionHandler.java
@@ -11,24 +11,49 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/**
+ * Creates error messages from exceptions.
+ * @author Toni Heinonen
+ * @author toni1.heinonen@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-   @Override
-   protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-       String error = "Malformed JSON request";
-       return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
-   }
 
-   private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
-       return new ResponseEntity<>(apiError, apiError.getStatus());
-   }
+    /**
+     * Creates a malformed JSON request message.
+     * @param ex http exception for not readable JSON
+     * @param headers headers of the request
+     * @param status https status for the error
+     * @param request request for the web client
+     * @return response entity for the error
+     */
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
 
-   @ExceptionHandler(EntityNotFoundException.class)
-   protected ResponseEntity<Object> handleEntityNotFound(
-           EntityNotFoundException ex) {
-       ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
-       apiError.setMessage(ex.getMessage());
-       return buildResponseEntity(apiError);
-   }
+    /**
+     * Builds the response entity with correct api error andstatus.
+     * @param apiError error container
+     * @return response entity for the error
+     */
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    /**
+     * Creates response entity when entity was not found.
+     * @param ex expection for entity not found
+     * @return response entity for the error
+     */
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException ex) {
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
 }
